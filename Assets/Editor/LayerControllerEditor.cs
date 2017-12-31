f1b2e69a5823bc90981437a46c6180d768063ba2
Luca Garbelli <lu.gbelli@hotmail.it>
2017-12-31 16:12:30 +0100

    - aggiunta bozza editor per layer controller

---
+++ Assets/Editor/LayerControllerEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Grid
+{
+    [CustomEditor(typeof(LayerController)), CanEditMultipleObjects]
+    public class LayerControllerEditor : Editor
+    {
+        LayerController layerCtrl;
+
+        private void OnEnable()
+        {
+            layerCtrl = (LayerController)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginVertical();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Layer", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+            GUILayout.Label("IsEditable", EditorStyles.boldLabel);
+            GUILayout.Label("GizmosColor", EditorStyles.boldLabel);
+            GUILayout.EndHorizontal();
+
+            foreach (Layer layer in layerCtrl.Layers)
+            {
+                EditorGUILayout.BeginHorizontal();
+                layer.Name = EditorGUILayout.TextField(layer.Name);
+                layer.IsEditable = EditorGUILayout.Toggle(layer.IsEditable);
+                layer.GizmosColor = EditorGUILayout.ColorField(layer.GizmosColor);
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.EndVertical();
+        }
+    }
+}
