4877c03d5a35870b18dc5ec2b68c3c8fa03f23a4
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-05 16:00:53 +0100

    - aggiunto editor a scanner collider. - da cotntrollare come vengono cancellati i link

--- Assets/Editor/ScannerColliderEditor.cs
+++ Assets/Editor/ScannerColliderEditor.cs
@@ -17,7 +17,34 @@ namespace Grid
 
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            EditorGUILayout.BeginVertical();
+
+            collider.ObjType = (ObjectType)EditorGUILayout.EnumPopup("Object Type", collider.ObjType);
+
+            EditorGUILayout.LabelField("Active Layers", EditorStyles.boldLabel);
+
+            EditorGUI.indentLevel = 1;
+            EditorGUILayout.BeginVertical();
+
+            if (MasterGrid.LayerCtrl == null)
+                return; //WORKAROUND !!
+            if (collider.ScannerLayers == null || collider.ScannerLayers.Count != MasterGrid.LayerCtrl.GetNumberOfLayers())
+            {
+                collider.ScannerLayers = new List<ScannerLayer>();
+                for (int i = 0; i < MasterGrid.LayerCtrl.GetNumberOfLayers(); i++)
+                {
+                    collider.ScannerLayers.Add(new ScannerLayer(MasterGrid.LayerCtrl.GetLayerAtIndex(i), false));
+                }
+            }
+            for (int i = 0; i < collider.ScannerLayers.Count; i++)
+            {
+                if (MasterGrid.LayerCtrl.GetLayerAtIndex(i).IsEditable)
+                    collider.ScannerLayers[i].Active = EditorGUILayout.Toggle(collider.ScannerLayers[i].Layer.Name, collider.ScannerLayers[i].Active);
+            }
+
+            EditorGUILayout.EndVertical();
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.EndVertical();
         }
     }
 }
