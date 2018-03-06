b47e2d73d0db004515d5d770dc4415c0ace17c3d
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-06 12:23:20 +0100

    Spostato GridInput

--- Assets/Scripts/GridInput.cs
+++ Assets/Scripts/GridInput.cs
@@ -5,10 +5,10 @@ namespace Grid {
     public class GridInput
     {
         static Ray mouseProjection;
-        static Plane gridLevel = new Plane(DataManager.GridDataInstance.Normal, DataManager.GridDataInstance.Normal.z); //TODO: da adattare alla griglia
+        static Plane gridLevel = new Plane(DataManager.GridDataInstance.Normal, DataManager.GridDataInstance.Normal.z - 1); //TODO: da adattare alla griglia
         public static Vector3 PointerPosition { get { return MousePositionOnGridPlane(); } }
 
-        static Vector3 MousePositionOnGridPlane()
+        public static Vector3 MousePositionOnGridPlane()
         {
             Vector3 currentMousePosition = Event.current.mousePosition;
 
